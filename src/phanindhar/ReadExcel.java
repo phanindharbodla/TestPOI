fca59345816a1de86a3d877ceeddcebc6957ef58
phanindharbodla <springbull2020@gmail.com>
2013-06-02 04:15:08 +0530

    First commit

---
+++ src/phanindhar/ReadExcel.java
@@ -0,0 +1,45 @@
+/*
+ * Reades the Excel file and Prints Records out of it .
+ */
+package phanindhar;
+
+/**
+ *
+ * @author Phanindhar
+ */
+import java.io.*;
+
+public class ReadExcel {
+
+    private final static int EXIT_STATUS_INPUT_FILE_NOT_FOUND = 1;
+    private final static int EXIT_STATUS_INPUT_FILE_IO_EXCEPTION = 2;
+    private static InputStream inputStream;
+    private static File inputFile = null;
+
+    public static void main(String[] args) {
+
+        String inputFileName = args[0];
+        try {
+            inputFile = new File(inputFileName);
+            inputStream = getInputStream(inputFile);
+        } catch (FileNotFoundException e) {
+            System.err.printf("error.file.notFound", e);
+            System.exit(EXIT_STATUS_INPUT_FILE_NOT_FOUND);
+        }
+        ExcelConverter conv = new ExcelConverter();
+        try {
+            if (inputFileName.toLowerCase().endsWith(".xls")) {
+                conv.processXlsFile(0,inputStream,System.out);
+            } else {
+                conv.processXlsxFile(0,inputStream,System.out);
+            }
+        } catch (IOException e) {
+            System.err.printf("error.file.ioException", e);
+            System.exit(EXIT_STATUS_INPUT_FILE_IO_EXCEPTION);
+        }
+    }
+
+    private static InputStream getInputStream(File file) throws FileNotFoundException {
+        return new FileInputStream(file);
+    }
+}
